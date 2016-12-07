474bbfd347ec71cb263969e91586ac618856898f
Maxime Desclefs <maxime.desclefs@gmail.com>
2016-12-07 20:26:46 +0100

    First implementation works

---
+++ ListImplementation.hs
@@ -0,0 +1,56 @@
+module ListImplementation where 
+
+import Data.List
+import Utils
+-- Position Definition
+type Path = (Position, Position)
+type Positions = [Position]
+data Position = Position { x :: Int
+                         , y :: Int
+                         } deriving (Eq)
+
+instance Show Position where
+    show position =  "(" ++ show (x position) ++ "," ++ show (y position) ++ ")"
+
+-- Board Definition
+
+-- data Board = Board [[Char]] deriving (Read) 
+type Matrix = [[Char]]
+data Board = Board  { width :: Int
+                    , height :: Int
+                    , entrance :: Position --Maybe if findEntrance
+                    , exits :: [Position]
+                    , walls :: [Position]
+                    } deriving (Show)
+
+getComposant :: Board -> Position -> Char
+getComposant board position 
+    | position == (entrance board) = '*'
+    | position `elem` (exits board) = '@'
+    | position `elem` (walls board) = 'X'
+    | otherwise = ' '
+
+instance Read Board where
+    readsPrec _ = parseBoard
+
+parseBoard :: String -> [(Board, String)]
+parseBoard txtBoard = [(Board width height entrance exits walls, "")]
+                    where
+                            board = lines txtBoard
+                            width = length (board !! 0) + 1
+                            height = length (board) + 1
+                            -- entrance = findEntrance board 0
+                            entrance = (findChar '*' board 0 []) !! 0
+                            exits = findChar '@' board 0 []
+                            walls = findChar 'X' board 0 []
+
+findEntrance :: Matrix -> Int -> Maybe Position
+findEntrance board y 
+    | (length board <= y) = Nothing
+    | (Utils.findElem '*' (board !! y) /= -1) = Just (Position (Utils.findElem '*' (board !! y)) y)
+    | otherwise = findEntrance board (y+1)
+
+findChar :: Char -> Matrix -> Int -> [Position] -> [Position]
+findChar pattern board y positions
+    | (length board <= y) = positions
+    | otherwise = findChar pattern board (y+1) (positions++(map (\x -> (Position x y)) (elemIndices pattern (board !! y))))
--- skeleton2.hs
+++ skeleton2.hs
@@ -1,20 +1,19 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
 
 import qualified System.Environment
 
-import qualified Utils
+import qualified ListImplementation
+--- import qualified TreeImplementation
 import Data.List
 import Data.Maybe
 -- MAYBE ON SHORTEST
+
 main :: IO ()
 main = do [path] <- System.Environment.getArgs
           maze <- readFile path
-          -- putStr $ show $ entrance $ read (maze :: Utils.Board)
-          putStr $ show $ (shortest $ (read maze::Utils.Board):: Utils.Positions)
-
-
-
+          -- putStr $ show $ entrance $ read (maze :: ListImplementation.Board)
+          putStr $ show $ (shortest $ (read maze::ListImplementation.Board):: ListImplementation.Positions)
+          -- putStr $ show $ (showSolution $ (read maze::ListImplementation.Board) ([]::ListImplementation.Positions))
 
 class (Read board, Show position) => Maze board position where
     entrance :: board -> position
@@ -23,26 +22,30 @@ class (Read board, Show position) => Maze board position where
     shortest :: board -> [position]
     shortest' :: board -> [(position, position)] -> [(position,position)] -> [(position,position)]
     computePath :: board -> (position, position) -> [(position, position)] -> [position] -> [position]
+    -- longest :: board -> [position]
+    -- longest' :: board -> [[position]]
+    showSolution :: board -> [position] -> String
 
-instance Maze Utils.Board Utils.Position where
-    entrance (Utils.Board _ _ entrance _ _) = entrance
-    exits (Utils.Board _ _ _ exits _) =  exits
-    neighbourghs (Utils.Board _ _ _ _ walls) (Utils.Position x y) = filter ( `notElem` walls ) [   Utils.Position x (y-1),  
-                                                                                                      Utils.Position x (y+1), 
-                                                                                                      Utils.Position (x-1) y, 
-                                                                                                      Utils.Position (x+1) y ]
-    shortest board = computePath board initPosition (reverse (shortest' board initQueue initPath)) []
+
+instance Maze ListImplementation.Board ListImplementation.Position where
+    entrance (ListImplementation.Board _ _ entrance _ _) = entrance
+    exits (ListImplementation.Board _ _ _ exits _) =  exits
+    neighbourghs (ListImplementation.Board _ _ _ _ walls) (ListImplementation.Position x y) = filter ( `notElem` walls ) [   ListImplementation.Position x (y-1),  
+                                                                                                   ListImplementation.Position x (y+1), 
+                                                                                                   ListImplementation.Position (x-1) y, 
+                                                                                                   ListImplementation.Position (x+1) y ]
+    shortest board = computePath board initPosition resultPath []
                      where 
                         initQueue = [(entrance board, entrance board)]
-                        initPosition = initQueue !! 0
                         initPath = []
+                        resultPath = (shortest' board initQueue initPath)
+                        initPosition = resultPath !! 0
 
     shortest' board (x:xs) path 
         | (actualPosition) `elem` (exits board) = newPath
-        | length (x:xs) == 0 = [] 
+        | (1+length xs) == 0 = [] 
         | otherwise = shortest' board newQueue newPath
         where
-            -- newQueue = xs++(snd x, (neighbourghs board x))
             actualPosition = snd x
             neighbourghsTupple = (concatMap (\next -> [(actualPosition,next)]) (neighbourghs board actualPosition))
             alreadyVisit = map snd path
@@ -50,10 +53,21 @@ instance Maze Utils.Board Utils.Position where
             newPath = x:path
 
     computePath board position tupplePath path
-        | ((fst position) == (entrance board)) = reverse (path)
+        | ((fst position) == (entrance board)) = path
         | otherwise = computePath board nextPositionTupple newTupplePath newPath
         where 
             nextPositionTupple = (filter ((==fst position).snd) tupplePath) !! 0
             nextPosition = fromJust (elemIndex nextPositionTupple (tupplePath))
             newTupplePath = drop nextPosition tupplePath
             newPath = (fst nextPositionTupple):path
+
+    -- longest board = longest' board initPathList
+
+    -- longest' board pathList
+    --     | (actualPosition) `elem` (exits board) = pathList
+
+    
+    showSolution board path = do
+        (y) <- [1..(ListImplementation.height board)]
+        (x) <- [1..(ListImplementation.width board)]
+        return $ ListImplementation.getComposant board (ListImplementation.Position x y)
